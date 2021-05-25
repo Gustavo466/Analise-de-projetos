d249ba23bf0ef84c99562370202e53162bae067c
Gustavo466 <84213274+Gustavo466@users.noreply.github.com>
2021-05-25 14:56:05 -0300

    Add files via upload

--- painel.php
+++ painel.php
@@ -3,40 +3,130 @@
     $login = $_SESSION[ 'login' ];
     $tipo = $_SESSION[ 'tipo' ];
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="utf-8" />
-        <title>Atividades</title>
+        <title>SmartMOB</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;600&family=Raleway&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="principal.css" />
         <script src="principal.js"></script>
         <script>
             sessionStorage.setItem( "login", '<?php echo $login; ?>' );
             sessionStorage.setItem( "tipo", '<?php echo $tipo; ?>' );
+            window.onload = function(  ){
+                if( sessionStorage.getItem( "tipo" ) != 'cliente' ){
+                    var escolha = document.getElementById( 'cad-imo' );
+                    escolha.style.display = 'none';
+                }
+            }
         </script>
     </head>
     <body>
-    <header>
-            <a href="index.html"><img src="logo.png"/></a>
-            <nav class="menu">
-                <a href="index.html">Página inicial</a>
-                <a href="sobre.html">Sobre nós</a>
+        <header>
+            <nav class="menu container">
+                <a href="index.html" class="logo">
+                    <h3>Smart<span>MOB</span></h3>
+                </a>
+                <ul class="links">
+                    <li><a href="#catalogo">Catálogo</a></li>
+                    <li><a href="#sobre-nos">Sobre nós</a></li>
+                    <li><a href="#contato">Contato</a></li>
+                </ul>
+                <ul class="logar">
+                    <li><a onclick="tipo( 'cad' )">Cadastrar</a></li>
+                    <li><a onclick="tipo( 'log' )">Logar</a></li>
+                </ul>
             </nav>
-            <section class="menu">
-                <a onclick="tipo( 'cad' )">Cadastrar</a>
-                <a onclick="tipo( 'log' )">Logar</a>
-            </section>
         </header>
-        <main class="corpo">
-            <h1>Bem vindo</h1>
+        <section class="banner">
+            <h1>Encontre o lugar <br> perfeito para morar,<br> aqui mesmo na SmartMOB!</h1>
+        </section>
+        <main class="container">
+            <br/><br/>
+            <nav class="menu container" id="cad-imo">
+                <ul class="logar">
+                    <li><a href="cadastra-imovel.html">Cadastrar novo imóvel</a></li>
+                </ul>
+            </nav>
+            <section class="catalogo" id="catalogo">
+                <h1>Catálogo</h1>
+                <a href="catalogo.php">Ver todos</a>
+                <div class="box-homes">
+                    <div class="single-home">
+                        <img src="casa-mk.jpg" alt="">
+                        <h5 class="preco">R$ 150.000</h5>
+                        <div class="row-info">
+                            <h4 class="bairro">Tatuapé</h4>
+                            <div class="inside">
+                                <p class="quartos">4</p>
+                                <p class="banheiros">3</p>
+                            </div>
+                        </div>
+                        <h6 class="cidade">São Paulo, SP</h6>
+                    </div>
+                    <div class="single-home">
+                        <img src="casa-mk.jpg" alt="">
+                        <h5 class="preco">R$ 150.000</h5>
+                        <div class="row-info">
+                            <h4 class="bairro">Tatuapé</h4>
+                            <div class="inside">
+                                <p class="quartos">4</p>
+                                <p class="banheiros">3</p>
+                            </div>
+                        </div>
+                        <h6 class="cidade">São Paulo, SP</h6>
+                    </div>
+                    <div class="single-home">
+                        <img src="casa-mk.jpg" alt="">
+                        <h5 class="preco">R$ 150.000</h5>
+                        <div class="row-info">
+                            <h4 class="bairro">Tatuapé</h4>
+                            <div class="inside">
+                                <p class="quartos">4</p>
+                                <p class="banheiros">3</p>
+                            </div>
+                        </div>
+                        <h6 class="cidade">São Paulo, SP</h6>
+                    </div>
+                    <div class="single-home">
+                        <img src="casa-mk.jpg" alt="">
+                        <h5 class="preco">R$ 150.000</h5>
+                        <div class="row-info">
+                            <h4 class="bairro">Tatuapé</h4>
+                            <div class="inside">
+                                <p class="quartos">4</p>
+                                <p class="banheiros">3</p>
+                            </div>
+                        </div>
+                        <h6 class="cidade">São Paulo, SP</h6>
+                    </div>
+                </div>
+            </section>
+            <section class="contato" id="contato">
+                <h1>Tem alguma dúvida?<br>Deixe nós ajudarmos você</h1>
+                <div class="email-wrapper">
+                    <form>
+                        <input type="email" placeholder="youremail@gmail.com">
+                        <button>Enviar</button>
+                    </form>
+                </div>
+            </section>
         </main>
         <footer>
-            <p>
-                <h3>Contatos: </h3>
-                Telefone: (11) 99999-9999<br/>
-                E-mail:<a href="mailto:exemplo@examplo.com?subject=Assunto" >exemplo@examplo.com</a><br/><br/>
-                &copy;All rights reserved.<br/>
-            </p>
-        </footer>
-    </body>
+            <div class="infos">
+                <div class="container">
+                        <p>Telefone: <span>(11) 99999-9999</span></p>
+                        <p>E-mail: <a href="mailto:exemplo@examplo.com?subject=Assunto"> exemplo@examplo.com</a></p>
+                    </div>
+                </div>
+                <div class="copy">
+                    <div class="container">
+                        &copy;All rights reserved.
+                    </div>
+                </div>
+            </footer>
+        </body>
 </html>
--- principal.js
+++ principal.js
@@ -4,37 +4,11 @@ function tipo( tip ){
     window.location.href = 'entrar.html';
 }
 
-function entrar(  ){
-    if ( sessionStorage.getItem( 'tipo' ) ){
-        var tipo = sessionStorage.getItem( 'tipo' );
-        if( tipo == 'cad' ){
-            cadastro(  );
-        }
-        else if( tipo == 'log' ){
-            login(  );
-        }
-    }
-}
-
-function login(  ){
-    var login = document.getElementById('login');
-    login.style.display = 'block';
-    var entrada = document.getElementById('entrada');
-    entrada.style.display = 'none';
-}
-
-function cadastro(  ){
-    var cadastro = document.getElementById('cadastro');
-    cadastro.style.display = 'block';
-    var entrada = document.getElementById('entrada');
-    entrada.style.display = 'none';
-}
-
 function gerente( ){
     var tipo = document.getElementById('tipo');
     tipo.value = 'gerente';
     var formulario = document.getElementById('form');
-    formulario.style.display = 'block';
+    formulario.style.display = 'flex';
     var escolha = document.getElementById('escolha');
     escolha.style.display = 'none';
 }
@@ -43,7 +17,7 @@ function corretor(  ){
     var tipo = document.getElementById('tipo');
     tipo.value = 'corretor';
     var formulario = document.getElementById('form');
-    formulario.style.display = 'block';
+    formulario.style.display = 'flex';
     var escolha = document.getElementById('escolha');
     escolha.style.display = 'none';
 }
@@ -52,7 +26,7 @@ function cliente( ){
     var tipo = document.getElementById('tipo');
     tipo.value = 'cliente';
     var formulario = document.getElementById('form');
-    formulario.style.display = 'block';
+    formulario.style.display = 'flex';
     var escolha = document.getElementById('escolha');
     escolha.style.display = 'none';
 }
